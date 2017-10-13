0cd1252c406f5a5a82f28f78f5e8f6350b3e059c
user <kenyesh@gmail.com>
2017-10-13 13:33:41 -0400

    calibration working

--- calibrateCamera.py
+++ calibrateCamera.py
@@ -3,6 +3,7 @@ import cv2
 import glob
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import pickle
 #%matplotlib inline
 
 images = glob.glob('camera_cal/*.jpg')
@@ -14,7 +15,7 @@ rows = 6;
 cols = 9;
 
 objp = np.zeros((rows*cols,3),np.float32)
-objp[:,:2] = np.mgrid[0:rows,0:cols].T.reshape(-1,2)
+objp[:,:2] = np.mgrid[0:cols,0:rows].T.reshape(-1,2)
 
 for idx, fname in enumerate(images):
     img = cv2.imread(fname)
@@ -35,6 +36,23 @@ for idx, fname in enumerate(images):
         #cv2.imshow('img', img)
         #cv2.waitKey(500)
 
-print("Done")
+print("Done Finding Corners")
+
+img = cv2.imread('camera_cal/calibration1.jpg')
+img_size = (img.shape[1], img.shape[0])
+
+# Do camera calibration given object points and image points
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size,None,None)
+
+dst = cv2.undistort(img, mtx, dist, None, mtx)
+cv2.imwrite('cal_out/test_undist.jpg',dst)
+
+# Save the camera calibration result for later use (we won't worry about rvecs / tvecs)
+dist_pickle = {}
+dist_pickle["mtx"] = mtx
+dist_pickle["dist"] = dist
+pickle.dump( dist_pickle, open( "cal_out/cal.p", "wb" ) )
+
+
 
 cv2.destroyAllWindows()
